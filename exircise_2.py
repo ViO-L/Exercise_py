a42679607d3883d56964cc735775765bb973866d
Victor <lomako221@mail.ru>
2024-01-18 20:49:55 +0300

    added inf

--- exircise_2.py
+++ exircise_2.py
@@ -11,25 +11,37 @@ var3 = '2 3 4 5' # элементы второго множества через
 На выходе:
 3 5
 '''
-# import random
-# var2 = '11 20 33 40 50'
-# var3 = '10 20 30 40 50'
-# var_1 = '5 4'
-# unical_number = (set(var3.replace(" ", "")).intersection(set(var2.replace(" ", ""))))
-# print(unical_number)
-# print_list = list(unical_number)
+var1 = '5 5'
+var2 = '10 20 30 40 50'
+var3 = '10 20 30 40 50'
+# delimitr = " "
+# split_string = var3.split(delimitr)
+# split_string2 = var2.split(delimitr)
+# ununical_number = (set(split_string).intersection(set(split_string2)))
+# print_list = list(ununical_number)
 # unical_number_sorted = sorted(print_list)
 # result = " ".join(unical_number_sorted)
 # print(result)
 
-var2 = '10 20 30 40 50'
-var3 = '10 20 30 40 50'
-delimitr = " "
-split_string = var3.split(delimitr)
-split_string2 = var2.split(delimitr)
-ununical_number = (set(split_string).intersection(set(split_string2)))
-print_list = list(ununical_number)
-unical_number_sorted = sorted(print_list)
-result = " ".join(unical_number_sorted)
-print(result)
+mol = [int(x) for x in var1.split()]
+n = mol[0]
+m = mol[1]
+set_1 = set()
+set_2 = set()
+list_1 = list()
+a = [int(x) for x in var2.split()]
+k = set(a)
+for i in k:
+   set_1.add(i)
+b = [int(x) for x in var3.split()]
+k1 = set(b)
+for i in k1:
+   set_2.add(i)
+lok = set_1 & set_2
+kool = list(lok)
+kool.sort()
+for i in kool:
+   print(i, end=' ')
+
+
 
